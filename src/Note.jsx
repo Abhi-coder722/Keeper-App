c3280b454bc76bd8508152af793d1a5c30a4f4f3
Abhi-Coder722 <072abhi@gmail.com>
2023-06-14 14:41:19 +0200

    notes added

--- src/Note.jsx
+++
@@ -1,4 +0,0 @@
-function Note(){
-return (<div><h1>This is the note title</h1> <p>This is the title content</p></div>);
-}
-export default Note;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "./compoents/App";
 //3. Create a Header.jsx component that renders a <header> element
 //to show the Keeper App name in an <h1>.
 //4. Create a Footer.jsx component that renders a <footer> element
